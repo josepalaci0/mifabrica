f8514792d95231d08cccc13ea8d891741f41dfc3
josepalaci0 <92130994+josepalaci0@users.noreply.github.com>
2022-08-12 20:26:10 -0500

    buttons config

--- src/components/home/search/Search.js
+++ src/components/home/search/Search.js
@@ -1,8 +1,10 @@
-export const Search = (data) =>{
+
+export const Search = (data) => {
     let img = ''
-    const configuracion =(<div id="configuracion">
-        <img src="https://i.pinimg.com/originals/b0/dc/b1/b0dcb1dabc27c57ca4de2fcdfbc0915a.png"/>
-        <img src="https://w7.pngwing.com/pngs/627/693/png-transparent-computer-icons-user-user-icon-thumbnail.png"/>
+    const configuracion = (<div id="configuracion">
+        <img alt="config-perfil" id="config-perfil" src="https://cdn-icons-png.flaticon.com/512/17/17004.png" />
+        <img alt="config-sitio" id="config-sitio" src="https://i.pinimg.com/originals/b0/dc/b1/b0dcb1dabc27c57ca4de2fcdfbc0915a.png" />
+        <img on onClick={() =>localStorage.removeItem('login')} alt="config-logoff" id="config-logoff" src="https://cdn-icons-png.flaticon.com/512/56/56805.png" />
     </div>)
     return configuracion
 }
